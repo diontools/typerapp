064f4577c7ff49ad48ea55c44738f241909fc60b
diontools <diontools@users.noreply.github.com>
2019-03-26 14:38:06 +0900

    add HttpJson

--- fx/effects/Http.ts
+++ fx/effects/Http.ts
@@ -29,3 +29,19 @@ export const HttpText = new Effect<HttpProps, { text: string }, { req: HttpProps
     },
     (action, props, runner) => [runner, { action, req: props }]
 )
+
+export const HttpJson = new Effect<HttpProps, { json: unknown }, { req: HttpProps }>(
+    (props, dispatch) => {
+        (Array.isArray(props.req)
+            ? fetch(props.req[0], props.req[1])
+            : fetch(props.req)
+        )
+            .then(response => {
+                if (!response.ok) throw response
+                return response.json()
+            })
+            .then(json => dispatch(props.action[0], { ...props.action[1], json }))
+            .catch(error => { throw error })
+    },
+    (action, props, runner) => [runner, { action, req: props }]
+)
